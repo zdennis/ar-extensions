395453894b5616103af88402e9e0c89994b686f2
zachdennis <zachdennis@5128a5ed-121c-0410-8d5c-98af306bc9be>
2006-08-29 23:38:29 +0000

    removed unneeded files start.rb and tests/debug.log
    git-svn-id: svn+ssh://rubyforge.org/var/svn/arext/trunk@2 5128a5ed-121c-0410-8d5c-98af306bc9be

--- start.rb
+++
@@ -1,8 +0,0 @@
-require 'boot'
-
-config = YAML.load( IO.read( 'config/database.yml' ) )
-ActiveRecord::Base.establish_connection( config['development'] )
-
-class Topic < ActiveRecord::Base ; end
-class Developer < ActiveRecord::Base ; end
-
