00398911a166bc4fd4708d9e35daabcc6963a20e
zachdennis <zachdennis@5128a5ed-121c-0410-8d5c-98af306bc9be>
2006-10-16 22:55:02 +0000

    Added ignore for any *.log files in the toplevel directory. Added a Rakefile to runs two rake tasks and updated init.rb
    git-svn-id: svn+ssh://rubyforge.org/var/svn/arext/trunk@16 5128a5ed-121c-0410-8d5c-98af306bc9be

--- base-extensions/init.rb
+++ base-extensions/init.rb
@@ -1,3 +1,5 @@
+begin ; require 'active_record' ; rescue; require 'rubygems'; require 'active_record'; end
+
 dir = File.dirname( __FILE__ )
 ar_base_path = File.join( dir, 'lib', 'active_record_base' )
 db_adapters_path = File.join( dir, 'lib', 'adapters' )
