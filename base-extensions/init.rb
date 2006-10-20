a7490d2c8771315a69180e3ea7b23ec32c3fd43c
zachdennis <zachdennis@5128a5ed-121c-0410-8d5c-98af306bc9be>
2006-10-20 03:41:16 +0000

    Removed commented line that required temporary table support, which is not currently implemented.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/arext/trunk@21 5128a5ed-121c-0410-8d5c-98af306bc9be

--- base-extensions/init.rb
+++ base-extensions/init.rb
@@ -6,7 +6,6 @@ db_adapters_path = File.join( dir, 'lib', 'adapters' )
 
 require File.join( ar_base_path, 'import' )
 require File.join( ar_base_path, 'finders' )
-#require File.join( ar_base_path, 'temporary_table' )
 
 require File.join( db_adapters_path, 'abstract_adapter' )
 require File.join( db_adapters_path, 'mysql_adapter' )
