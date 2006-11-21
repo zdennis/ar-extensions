dd30a43cbc2d20def6fe8efc89fa9ee71d88938c
zachdennis <zachdennis@5128a5ed-121c-0410-8d5c-98af306bc9be>
2006-11-21 03:20:22 +0000

    Updated init.rb to reflect new path changes from the last commit.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/arext/trunk@36 5128a5ed-121c-0410-8d5c-98af306bc9be

--- ar-extensions/init.rb
+++ ar-extensions/init.rb
@@ -4,13 +4,11 @@ dir = File.dirname( __FILE__ )
 require File.join( dir, 'lib', 'extensions' )
 require File.join( dir, 'lib', 'fulltext' )
 
-
-ar_base_path = File.join( dir, 'lib', 'active_record_base' )
 db_adapters_path = File.join( dir, 'lib', 'adapters' )
 
-require File.join( ar_base_path, 'import' )
-require File.join( ar_base_path, 'finders' )
+require File.join( dir, 'lib', 'import' )
+require File.join( dir, 'lib', 'finders' )
 
 require File.join( db_adapters_path, 'abstract_adapter' )
-require File.join( db_adapters_path, 'mysql_adapter' )
+require File.join( db_adapters_path,'mysql_adapter' )
 
