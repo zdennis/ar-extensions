2df81391697fc47a8acf9b7ca6489986ec6009ef
zachdennis <zachdennis@5128a5ed-121c-0410-8d5c-98af306bc9be>
2006-12-14 21:09:03 +0000

    Refactored FindToCSV support
    git-svn-id: svn+ssh://rubyforge.org/var/svn/arext/trunk@45 5128a5ed-121c-0410-8d5c-98af306bc9be

--- ar-extensions/lib/csv.rb
+++ ar-extensions/lib/csv.rb
@@ -1,18 +1,30 @@
 require 'faster_csv'
 
+=begin
+TODO - support to_csv( filename, options )
+OPTIONS TO SUPPORT:
+ :headers 
+ :exclude_columns
+ :include_columns
+=end
+
 module ActiveRecord::Extensions::FindToCSV
+  ALIAS_FOR_FIND = :_original_find_before_arext
+
   def self.included( cl )
-    cl.instance_eval "
-      alias :_original_find :find
-      def find( *args )
-        results = _original_find( *args )
-        results.extend( ArrayExtension ) if results.is_a?( Array )
-        results
-      end
-    "
+    cl.instance_eval "alias #{ALIAS_FOR_FIND} :find"
+    cl.extend( ClassMethods )
   end
 
-  module ArrayExtension
+  module ClassMethods
+    def find( *args )
+      results = self.send( ALIAS_FOR_FIND, *args )
+      results.extend( InstanceMethods ) if results.is_a?( Array )
+      results
+    end
+  end
+
+  module InstanceMethods
     def to_csv( filename )
       csv = FasterCSV.generate do |csv|
         csv << self.first.attributes.sort.map{ |arr| arr.first }
