974685e01d6693cb8c83081dde51f83a63644d91
zachdennis <zachdennis@5128a5ed-121c-0410-8d5c-98af306bc9be>
2006-12-18 01:20:55 +0000

    Added postgresql schema migration file. Moved rake task 'db:prepare_<adapter>' to 'db:test:prepare_<adapter>'.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/arext/trunk@50 5128a5ed-121c-0410-8d5c-98af306bc9be

--- ar-extensions/db/migrate/postgresql_schema.rb
+++ ar-extensions/db/migrate/postgresql_schema.rb
@@ -1,19 +1,4 @@
 ActiveRecord::Schema.define do
- 
-  create_table :test_myisam, :options=>'ENGINE=MyISAM', :force=>true do |t|
-    t.column :my_name, :string, :null=>false
-    t.column :description, :string
-  end
-  
-  create_table :test_innodb, :options=>'ENGINE=InnoDb', :force=>true do |t|
-    t.column :my_name, :string, :null=>false
-    t.column :description, :string
-  end
-
-  create_table :test_memory, :options=>'ENGINE=Memory', :force=>true do |t|
-    t.column :my_name, :string, :null=>false
-    t.column :description, :string
-  end
   
   create_table :topics, :force=>true do |t|
     t.column :title, :string, :null=>false
@@ -28,8 +13,8 @@ ActiveRecord::Schema.define do
     t.column :parent_id, :integer
     t.column :type, :string    
   end
-  
-  create_table :projects, :force=>true do |t|
+
+    create_table :projects, :force=>true do |t|
     t.column :name, :string
     t.column :type, :string    
   end
@@ -41,11 +26,10 @@ ActiveRecord::Schema.define do
     t.column :updated_at, :datetime
   end
   
-  create_table :books, :options=>'ENGINE=MyISAM', :force=>true do |t|
+  create_table :books, :force=>true do |t|
     t.column :title, :string, :null=>false
     t.column :publisher, :string, :null=>false
     t.column :author_name, :string, :null=>false
-  end
-  execute "ALTER TABLE books ADD FULLTEXT( `title`, `publisher`, `author_name` )"
-  
+  end  
+
 end
