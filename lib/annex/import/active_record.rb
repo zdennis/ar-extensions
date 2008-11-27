6c26d5d98525f3f1a60df18596022d62fbdbd69f
Zach Dennis <zach.dennis@gmail.com>
2008-11-27 17:38:52 -0500

    Refactored import examples.

--- lib/annex/import/active_record.rb
+++ lib/annex/import/active_record.rb
@@ -13,11 +13,9 @@ class ActiveRecord::Base
     if args.size == 1
       columns = column_names.dup
       instances = args.first
-      values = instances.map{ |model| columns.map{ |column| model.attributes[column.to_s] } }
     elsif args.size == 2 && args.last.is_a?(Array)
       if args.last.first.kind_of?(ActiveRecord::Base)
         columns, instances = args
-        values = instances.map{ |model| columns.map{ |column| model.attributes[column.to_s] } }
       else
         columns, values = args
       end
@@ -25,23 +23,14 @@ class ActiveRecord::Base
       options.merge! args.pop
       columns = column_names.dup
       instances = args.first
-      values = instances.map{ |model| columns.map{ |column| model.attributes[column.to_s] } }
     elsif args.size == 3 && args.last.is_a?(Hash)
       if args[1].first.kind_of?(ActiveRecord::Base)
         options.merge! args.pop
         columns, instances = args
-        values = instances.map{ |model| columns.map{ |column| model.attributes[column.to_s] } }
       else
         options.merge! args.pop
         columns, values = args
       end
-    end
-    
-    sql_statements = generate_sql :insert_into do |sql|
-      sql.table = quoted_table_name
-      sql.columns = columns.map{ |name| connection.quote_column_name(name) }
-      sql.values = values.map{ |rows| rows.map{ |field| connection.quote(field, columns_hash[columns[rows.index(field)]]) } }
-      sql.options = options
     end
 
     if options[:validate]
@@ -56,14 +45,26 @@ class ActiveRecord::Base
         end
       end
       invalid_instances = instances.select{ |instance| !instance.valid? }
+      instances -= invalid_instances
+    end
+    
+    if instances
+      if instances.any?
+        values = instances.map{ |model| columns.map{ |column| model.attributes[column.to_s] } }
+      else
+        return ContinuousThinking::SQL::Result.new(:num_inserts => 0, :failed_instances => invalid_instances)
+      end
+    end
+    
+    sql_statements = generate_sql :insert_into do |sql|
+      sql.table = quoted_table_name
+      sql.columns = columns.map{ |name| connection.quote_column_name(name) }
+      sql.values = values.map{ |rows| rows.map{ |field| connection.quote(field, columns_hash[columns[rows.index(field)]]) } }
+      sql.options = options
     end
 
-    if invalid_instances.any?
-      return ContinuousThinking::SQL::Result.new(:num_inserts => 0, :failed_instances => invalid_instances)
-    end      
-
     sql_statements.each { |statement| connection.execute statement }
-    ContinuousThinking::SQL::Result.new(:num_inserts => values.size, :failed_instances => [])
+    ContinuousThinking::SQL::Result.new(:num_inserts => values.size, :failed_instances => invalid_instances)
   end
   
 end
--- specs/imports/import_spec.rb
+++ specs/imports/import_spec.rb
@@ -9,23 +9,49 @@ describe ActiveRecord, "importing data" do
   after(:each) do
     ActiveRecord::Base.connection.rollback_db_transaction
   end
-  
-  it "should import data with array of columns and values" do
+
+  it "should import data with columns and values" do
     lambda { 
-      Topic.import(%w(title author_name), [%w(Ruby Matz), %w(Lisp McCarthy)], :validate => true)
-    }.should change(Topic, :count).by(2)
-    Topic.find_by_title_and_author_name!('Ruby', 'Matz').should_not be_nil
-    Topic.find_by_title_and_author_name!('Lisp', 'McCarthy').should_not be_nil
-  end
-    
-  it "should not import any data when validation fails on at least one record with validations turned on" do
-    lambda { 
-      result = Topic.import(%w(title author_name), [%w(Ruby Matz), %w(Lisp)], :validate => true)
+      result = Topic.import(%w(title author_name), [["Ruby", nil], ["Lisp", nil]])
       result.num_inserts.should == 0
     }.should_not change(Topic, :count)
   end
-    
-  it "should import an array of model objects" do
+  
+  it "should import data with columns and values with validations turned on" do
+    lambda { 
+      Topic.import(%w(title author_name), [%w(Ruby Matz), %w(Lisp McCarthy)], :validate => true)
+    }.should change(Topic, :count).by(2)
+    Topic.find_by_title_and_author_name('Ruby', 'Matz').should_not be_nil
+    Topic.find_by_title_and_author_name('Lisp', 'McCarthy').should_not be_nil
+  end
+
+  it "should not import invalid data given columns and values with validations turned on" do
+    lambda { 
+      result = Topic.import(%w(title author_name), [%w(Ruby Matz), ["Lisp", nil]], :validate => true)
+      result.num_inserts.should == 1
+    }.should change(Topic, :count).by(1)
+    Topic.find_by_title_and_author_name("Ruby", "Matz").should_not be_nil
+  end
+  
+  it "should import invalid data given columns and values with validations turned off" do
+    lambda { 
+      result = Topic.import(%w(title author_name), [%w(Ruby Matz), ["Lisp", nil]], :validate => false)
+      result.num_inserts.should == 2
+    }.should change(Topic, :count).by(2)
+    Topic.find_by_title_and_author_name("Ruby", "Matz").should_not be_nil
+    Topic.find_by_title_and_author_name("Lisp", nil).should_not be_nil
+  end
+  
+  it "should import valid data given models" do
+    topics = [Topic.new(:title=>"Ruby", :author_name=>"Matz"), Topic.new(:title=>"Lisp", :author_name=>"McCarthy")]
+    lambda {
+      Topic.import topics
+    }.should change(Topic, :count).by(2)
+    Topic.find_by_title_and_author_name("Ruby", "Matz").should_not be_nil
+    Topic.find_by_title_and_author_name("Lisp", "McCarthy").should_not be_nil
+  end
+
+  it "should import valid models with validations turned on" do
     topics = [Topic.new(:title=>"Ruby", :author_name=>"Matz"), Topic.new(:title=>"Lisp", :author_name=>"McCarthy")]
     lambda {
       Topic.import topics, :validate => true
@@ -34,15 +60,26 @@ describe ActiveRecord, "importing data" do
     Topic.find_by_title_and_author_name("Lisp", "McCarthy").should_not be_nil
   end
 
-  it "should not import any data when validation fails on at least one model objects with validations turned on" do
-    topics = [Topic.new(:title=>"Ruby", :author_name=>"Matz"), Topic.new(:title=>"Lisp")]
-    lambda {
-      result = Topic.import topics, :validate => true
-      result.num_inserts.should == 0
-    }.should change(Topic, :count).by(0)
+  it "should not import invalid models with validations turned on" do
+    lambda { 
+      result = Topic.import [Topic.new, Topic.new, Topic.new(:title => "Ruby", :author_name => "Matz")], :validate => true
+      result.num_inserts.should == 1
+    }.should change(Topic, :count).by(1)
+    Topic.find_by_title_and_author_name("Ruby", "Matz").should_not be_nil
   end
 
-  it "should import data with an array of column names and an array of model objects" do
+  it "should import invalid models with validations turned off" do
+    lambda { 
+      topics = [Topic.new(:title => "Lisp"), Topic.new(:title => "Smalltalk"), Topic.new(:title => "Ruby", :author_name => "Matz")]
+      result = Topic.import topics, :validate => false
+      result.num_inserts.should == 3
+    }.should change(Topic, :count).by(3)
+    Topic.find_by_title_and_author_name("Smalltalk").should_not be_nil
+    Topic.find_by_title_and_author_name("Lisp").should_not be_nil
+    Topic.find_by_title_and_author_name("Ruby", "Matz").should_not be_nil
+  end
+
+  it "should import data given columns and models" do
     topics = [
       Topic.new(:title=>"Ruby", :author_name=>"Matz", :content => "abc"), 
       Topic.new(:title=>"Lisp", :author_name=>"McCarthy", :content => "123")]
@@ -54,26 +91,54 @@ describe ActiveRecord, "importing data" do
     Topic.find_by_title_and_author_name("Lisp", "McCarthy").should_not be_nil
   end
 
-  it "should not import data with an array of column names and an invalid model object" do
+  it "should import data given columns and models with validations turned on" do
     topics = [
-      Topic.new(:title=>"Ruby", :author_name=>"", :content => "abc"), 
+      Topic.new(:title=>"Ruby", :author_name=>"Matz", :content => "abc"), 
       Topic.new(:title=>"Lisp", :author_name=>"McCarthy", :content => "123")]
     lambda { 
-      result = Topic.import [:title, :author_name], topics, :validate => true
-      result.num_inserts.should == 0
-    }.should change(Topic, :count).by(0)
-  end
-  
-  it "should import data with an array of columns names and invalid model objects with validation turned off" do
-    topics = [
-      Topic.new(:title=>"Ruby", :author_name=>"", :content => "abc"), 
-      Topic.new(:title=>"Lisp", :author_name=>"McCarthy", :content => "123")]
-    lambda { 
-      result = Topic.import [:title, :author_name, :content], topics, :validate => false
+      result = Topic.import [:title, :author_name], topics, :validations => true
       result.num_inserts.should == 2
     }.should change(Topic, :count).by(2)
+    Topic.find_by_title_and_author_name("Ruby", "Matz").should_not be_nil
+    Topic.find_by_title_and_author_name("Lisp", "McCarthy").should_not be_nil
+  end
+
+  it "should not import invalid data given columns and models" do
+    topics = [
+      Topic.new(:title=>"Ruby", :author_name=>"", :content => "abc"), 
+      Topic.new(:title=>"Lisp", :author_name=>"", :content => "123"),
+      Topic.new(:title=>"Smalltalk", :author_name=>"Kay", :content => "xyz")]
+    lambda { 
+      result = Topic.import [:title, :author_name], topics
+      result.num_inserts.should == 1
+    }.should change(Topic, :count).by(1)
+    Topic.find_by_title_and_author_name_and_content("Smalltalk", "Kay", nil).should_not be_nil
+  end
+  
+  it "should not import invalid data given columns and models with validation turned on" do
+    topics = [
+      Topic.new(:title=>"Ruby", :author_name=>"", :content => "abc"), 
+      Topic.new(:title=>"Lisp", :author_name=>"", :content => "123"),
+      Topic.new(:title=>"Smalltalk", :author_name=>"Kay", :content => "xyz")]
+    lambda { 
+      result = Topic.import [:title, :author_name], topics, :validate => true
+      result.num_inserts.should == 1
+    }.should change(Topic, :count).by(1)
+    Topic.find_by_title_and_author_name_and_content("Smalltalk", "Kay", nil).should_not be_nil
+  end
+  
+  it "should import invalid data given columns and invalid models with validation turned off" do
+    topics = [
+      Topic.new(:title=>"Ruby", :author_name=>"", :content => "abc"), 
+      Topic.new(:title=>"Lisp", :author_name=>"", :content => "123"),
+      Topic.new(:title=>"Smalltalk", :author_name=>"Kay", :content => "xyz")]
+    lambda { 
+      result = Topic.import [:title, :author_name, :content], topics, :validate => false
+      result.num_inserts.should == 3
+    }.should change(Topic, :count).by(3)
     Topic.find_by_title_and_author_name_and_content("Ruby", "", "abc").should_not be_nil
-    Topic.find_by_title_and_author_name_and_content("Lisp", "McCarthy", "123").should_not be_nil
+    Topic.find_by_title_and_author_name_and_content("Lisp", "", "123").should_not be_nil
+    Topic.find_by_title_and_author_name_and_content("Smalltalk", "Kay", "xyz").should_not be_nil
   end
 end
 
@@ -88,7 +153,7 @@ describe ActiveRecord, "reporting on imported data" do
   end
     
   it "should report the number of instances that failed validation when given columns and values" do
-    result = Topic.import(%w(title author_name), [%w(Ruby Matz), %w(Lisp)], :validate => true)
+    result = Topic.import(%w(title author_name), [%w(Ruby Matz), ["Lisp", nil]], :validate => true)
     result.failed_instances.should have(1).instances
   end
   
