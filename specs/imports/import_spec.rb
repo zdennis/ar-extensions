b3589308759702f5626a78b609246d1b506f37c5
Zach Dennis <zach.dennis@gmail.com>
2008-11-27 12:45:21 -0500

    Refactored the import spec to group reporting related examples.

--- specs/imports/import_spec.rb
+++ specs/imports/import_spec.rb
@@ -1,7 +1,7 @@
 require File.expand_path(File.dirname(__FILE__)) + "/../spec_helper"
 require "annex/import/active_record"
 
-describe "ActiveRecord", "importing data" do
+describe ActiveRecord, "importing data" do
   before(:each) do
     ActiveRecord::Base.connection.begin_db_transaction
   end
@@ -24,23 +24,7 @@ describe "ActiveRecord", "importing data" do
       result.num_inserts.should == 0
     }.should_not change(Topic, :count)
   end
-  
-  it "should report the number of database inserts" do
-    result = Topic.import(%w(title author_name), [%w(Ruby Matz), %w(Lisp McCarthy)], :validate => true)
-    result.num_inserts.should == 2
-  end
-  
-  it "should report the number of instances that failed validation when given columns and values" do
-    result = Topic.import(%w(title author_name), [%w(Ruby Matz), %w(Lisp)], :validate => true)
-    result.failed_instances.should have(1).instances
-  end
-  
-  it "should report the number of instances that failed validation when given models" do
-    topics = [Topic.new(:title=>"Ruby"), Topic.new(:title=>"Lisp"), Topic.new(:title=>"Smalltalk", :author_name => "Kay")]
-    result = Topic.import topics
-    result.failed_instances.should have(2).instances
-  end
-  
+    
   it "should import an array of model objects" do
     topics = [Topic.new(:title=>"Ruby", :author_name=>"Matz"), Topic.new(:title=>"Lisp", :author_name=>"McCarthy")]
     lambda {
@@ -91,5 +75,32 @@ describe "ActiveRecord", "importing data" do
     Topic.find_by_title_and_author_name_and_content("Ruby", "", "abc").should_not be_nil
     Topic.find_by_title_and_author_name_and_content("Lisp", "McCarthy", "123").should_not be_nil
   end
+end
 
+
+describe ActiveRecord, "reporting on imported data" do
+  before(:each) do
+    ActiveRecord::Base.connection.begin_db_transaction
+  end
+  
+  after(:each) do
+    ActiveRecord::Base.connection.rollback_db_transaction
+  end
+    
+  it "should report the number of instances that failed validation when given columns and values" do
+    result = Topic.import(%w(title author_name), [%w(Ruby Matz), %w(Lisp)], :validate => true)
+    result.failed_instances.should have(1).instances
+  end
+  
+  it "should report the number of instances that failed validation when given models" do
+    topics = [Topic.new(:title=>"Ruby"), Topic.new(:title=>"Lisp"), Topic.new(:title=>"Smalltalk", :author_name => "Kay")]
+    result = Topic.import topics
+    result.failed_instances.should have(2).instances
+  end
+
+  it "should report the number of instances that failed validation when given columns and models" do
+    topics = [Topic.new(:title=>"Ruby"), Topic.new(:title=>"Lisp"), Topic.new(:title=>"Smalltalk", :author_name => "Kay")]
+    result = Topic.import [:title], topics, :validate => true
+    result.failed_instances.should have(2).instances
+  end  
 end
