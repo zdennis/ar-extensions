4af1d7a2725a388bf2f1d1f12e707d6d177f906f
Blythe Dunham <blythedunham@gmail.com>
2009-03-11 23:05:51 -0600

    Update tests to use _should_ format Update insert_select to take :from and :into options Change finder_options test to use regexp

--- ar-extensions/tests/test_finder_options.rb
+++ ar-extensions/tests/test_finder_options.rb
@@ -19,7 +19,7 @@ class FindersTest < Test::Unit::TestCase
     Book.delete_all
   end
   
-  def test_find_with_having
+  def test_find_with_having_should_add_having_option_to_sql
 
     create_books_and_topics
     
@@ -38,27 +38,53 @@ class FindersTest < Test::Unit::TestCase
     assert_equal 2, books.size
     
   end
-  
-  def test_finder_sql_to_string
+  def test_finder_sql_to_string_should_select_from_one_table
     book_sql = Book.finder_sql_to_string(:select => 'topic_id', :include => :topic)
-    assert_equal('SELECT topic_id FROM `books`', book_sql)
-    
-    book_sql = Book.finder_sql_to_string(:select => 'topic_id', :include => :topic, :conditions => 'topics.id is not null')
-    assert_equal("SELECT `books`.`id` AS t0_r0, `books`.`title` AS t0_r1, `books`.`publisher` AS t0_r2, `books`.`author_name` AS t0_r3, `books`.`created_at` AS t0_r4, `books`.`created_on` AS t0_r5, `books`.`updated_at` AS t0_r6, `books`.`updated_on` AS t0_r7, `books`.`topic_id` AS t0_r8, `books`.`for_sale` AS t0_r9, `topics`.`id` AS t1_r0, `topics`.`title` AS t1_r1, `topics`.`author_name` AS t1_r2, `topics`.`author_email_address` AS t1_r3, `topics`.`written_on` AS t1_r4, `topics`.`bonus_time` AS t1_r5, `topics`.`last_read` AS t1_r6, `topics`.`content` AS t1_r7, `topics`.`approved` AS t1_r8, `topics`.`replies_count` AS t1_r9, `topics`.`parent_id` AS t1_r10, `topics`.`type` AS t1_r11, `topics`.`created_at` AS t1_r12, `topics`.`updated_at` AS t1_r13 FROM `books`  LEFT OUTER JOIN `topics` ON `topics`.id = `books`.topic_id WHERE (topics.id is not null)", book_sql)
+    # current generated output looks like this now:
+    # SELECT topic_id FROM `books`
+
+    assert(/^SELECT\s/.match(book_sql))
+    assert_nil(/\sJOIN\s/.match(book_sql))
+    (Book.column_names - ['topic_id']).each do |col|
+      assert_nil(book_sql.match(Regexp.new("^SELECT .*[`\s]#{col}.*FROM")))
+    end
+
+    assert(/^SELECT\s.*topic_id.*FROM/.match(book_sql))
   end
   
-  def test_pre_sql
+  def test_finder_sql_to_string_should_generate_sql_with_joined_table
+    conditions = 'topics.id is not null'
+    book_sql = Book.finder_sql_to_string(:select => 'topic_id', :include => :topic, :conditions => conditions)
+    
+    assert(/^SELECT\s/.match(book_sql))
+    assert(/\sJOIN\s/.match(book_sql))
+    assert(book_sql.include?(conditions))
+
+    #assert that each column was included in the sql
+    Book.column_names.each do |col|
+      assert(book_sql.match(Regexp.new("^SELECT .*[`\s\.]#{col}.*FROM")))
+    end
+    
+    # current generated output looks like this now:
+    #"SELECT `books`.`id` AS t0_r0, `books`.`title` AS t0_r1, `books`.`publisher` AS t0_r2, `books`.`author_name` AS t0_r3, `books`.`created_at` AS t0_r4, `books`.`created_on` AS t0_r5, `books`.`updated_at` AS t0_r6, `books`.`updated_on` AS t0_r7, `books`.`topic_id` AS t0_r8, `books`.`for_sale` AS t0_r9, `topics`.`id` AS t1_r0, `topics`.`title` AS t1_r1, `topics`.`author_name` AS t1_r2, `topics`.`author_email_address` AS t1_r3, `topics`.`written_on` AS t1_r4, `topics`.`bonus_time` AS t1_r5, `topics`.`last_read` AS t1_r6, `topics`.`content` AS t1_r7, `topics`.`approved` AS t1_r8, `topics`.`replies_count` AS t1_r9, `topics`.`parent_id` AS t1_r10, `topics`.`type` AS t1_r11, `topics`.`created_at` AS t1_r12, `topics`.`updated_at` AS t1_r13 FROM `books`  LEFT OUTER JOIN `topics` ON `topics`.id = `books`.topic_id WHERE (topics.id is not null)"
+  end
+  
+  def test_pre_sql_should_ensure_pre_sql_option_is_added_to_beginning_of_sql
     book_sql = Book.finder_sql_to_string(:select => 'topic_id', :pre_sql => "/* BLAH */")
     assert(/^\/\* BLAH \*\/\sSELECT/.match(book_sql))
-    
+  end
+
+  def test_pre_sql_should_ensure_pre_sql_option_is_added_to_beginning_of_sql_with_eager_loading
     book_sql = Book.finder_sql_to_string(:select => 'topic_id', :pre_sql => "/* BLAH */", :include => :topic, :conditions => 'topics.id is not null')
     assert(/^\/\* BLAH \*\/\sSELECT/.match(book_sql))
-  end 
+  end
   
-  def test_post_sql
+  def test_pre_sql_should_ensure_post_sql_option_is_added_to_end_of_sql
     book_sql = Book.finder_sql_to_string(:select => 'topic_id', :post_sql => "/* BLAH */")
     assert(/\s\/\* BLAH \*\/$/.match(book_sql))
-    
+  end
+
+  def test_pre_sql_should_ensure_post_sql_option_is_added_to_end_of_sql_with_eager_loading
     book_sql = Book.finder_sql_to_string(:select => 'topic_id', :post_sql => "/* BLAH */", :include => :topic, :conditions => 'topics.id is not null')
     assert(/\s\/\* BLAH \*\/$/.match(book_sql))
   end
--- ar-extensions/tests/test_insert_select.rb
+++ ar-extensions/tests/test_insert_select.rb
@@ -1,3 +1,4 @@
+ENV["ARE_DB"] = 'mysql'
 require File.expand_path( File.join( File.dirname( __FILE__ ), 'test_helper') )
 
 class InsertSelectTest < Test::Unit::TestCase
@@ -8,7 +9,7 @@ class InsertSelectTest < Test::Unit::TestCase
   
   #define the duplicate key update for mysql for testing
   #add oracle, postgre, sqlite, etc when implemented
-  if ENV["ARE_DB"] = 'mysql'
+  if ENV["ARE_DB"] == 'mysql'
     DUPLICATE_UPDATE_STR = 'cart_items.updated_at=VALUES(`updated_at`), copies=VALUES(copies), book_id=VALUES(`book_id`)'
   else
     DUPLICATE_UPDATE_STR = [:updated_at, :copies, :book_id]
@@ -17,7 +18,7 @@ class InsertSelectTest < Test::Unit::TestCase
   def setup
     @connection = ActiveRecord::Base.connection
     @conditions = ['author_name like :author_name', {:author_name => 'Terry Brooks'}]
-    @select_columns = [:book_id, :shopping_cart_id, :copies, :updated_at, :created_at]
+    @into_columns = [:book_id, :shopping_cart_id, :copies, :updated_at, :created_at]
     @cart = ShoppingCart.create!(:name => 'My Shopping Cart')
     @time = Time.now - 2.seconds
     
@@ -31,11 +32,14 @@ class InsertSelectTest < Test::Unit::TestCase
   end
  
   #test simple insert select
-  def test_insert_select_simple
+  def test_insert_select_should_import_data_from_one_model_into_another
     assert_equal 0, Topic.count
 
     timestamp = Time.now
-    Topic.insert_select :book, {:select => ['title, author_name, ?', timestamp]}, {:select => [:title, :author_name, :updated_at]}
+    Topic.insert_select(
+      :from => :book,
+      :select => ['title, author_name, ?', timestamp],
+      :into => [:title, :author_name, :updated_at])
 
     books = Book.find :all, :order => 'title'
     topics = Topic.find :all, :order => 'title'
@@ -48,16 +52,33 @@ class InsertSelectTest < Test::Unit::TestCase
       assert_equal topic.updated_at.to_s, timestamp.to_s
     }
     
-    CartItem.insert_select(:book, 
-      {:select => ['books.id, ?, ?, ?, now()', @cart.to_param, 1, Time.now]}, 
-      {:select => [:book_id, :shopping_cart_id, :copies, :updated_at, :created_at]})
-         CartItem.insert_select(Book, 
-                             {:select => ['books.id, ?, ?, ?, now()', @cart.to_param, 1, Time.now]}, 
-                            {:select => 'cart_items.book_id, shopping_cart_id, copies, updated_at, created_at',
-                             :ignore => true }) 
+
+  end
+
+  def test_insert_select_should_import_data_from_one_model_into_another_ignoring_existing_data
+
+    time = Time.now - 4.seconds
+    #insert book data into cart
+    CartItem.insert_select(
+       :from => :book,
+       :select => ['books.id, ?, ?, ?, now()', @cart.to_param, 1, time],
+       :into => [:book_id, :shopping_cart_id, :copies, :updated_at, :created_at])
+
+    total = CartItem.count(:id, :conditions => ['shopping_cart_id = ? and updated_at = ?', @cart.to_param, time])
+    assert_equal 9, total, "Expecting 6 cart items. Instead got #{total}"
+
+    #insert the same data from book into the cart
+    CartItem.insert_select(:from => Book,
+                           :select => ['books.id, ?, ?, ?, now()', @cart.to_param, 1, Time.now],
+                           :into => 'cart_items.book_id, shopping_cart_id, copies, updated_at, created_at',
+                           :ignore => true )
+
+    #ensure that the data has not changed
+    total = CartItem.count(:id, :conditions => ['shopping_cart_id = ? and updated_at = ?', @cart.to_param, time])
+    assert_equal 9, total, "Expecting 6 cart items. Instead got #{total}"
   end
   
-  def test_insert_select_with_include(select_options={})
+  def test_insert_select_should_import_data_from_one_model_into_another_updating_existing_data(options_one={}, options_two={})
     
     fun_topic = Topic.create!(:title => 'Fun Books', :author_name => 'Big Bird')
     ok_topic = Topic.create!(:title => 'OK Books', :author_name => 'sloth')
@@ -67,11 +88,12 @@ class InsertSelectTest < Test::Unit::TestCase
     Book.update_all(['topic_id = ?', fun_topic], 'books.id <  3')
     Book.update_all(['topic_id = ?', ok_topic], 'books.id =  3')
     
-    CartItem.insert_select(:book, 
-      {:select => ['books.id, ?, ?, ?, ?', @cart.to_param, 1, @time, @time], 
+    CartItem.insert_select(
+      {:from => :book,
+       :select => ['books.id, ?, ?, ?, ?', @cart.to_param, 1, @time, @time],
+       :into => @into_columns,
        :conditions => ['topics.title = :title', {:title => 'Fun Books'}],
-       :include => :topic }.merge(select_options), 
-      {:select => @select_columns})
+       :include => :topic }.merge(options_one))
     
     validate_cart_items({:total => 2, :copies => 1 },
                         :conditions => ['topics.title in (?)', ['Fun Books']],
@@ -79,11 +101,13 @@ class InsertSelectTest < Test::Unit::TestCase
 
     #insert select with on duplicate key update written as a string
     new_time = Time.now
-    CartItem.insert_select(:book, 
-      {:select => ['books.id, ?, ?, ?, ?', @cart.to_param, 2, new_time, new_time], 
+    CartItem.insert_select(
+      {:from => :book,
+       :select => ['books.id, ?, ?, ?, ?', @cart.to_param, 2, new_time, new_time],
+       :into => @into_columns,
        :conditions => ['topics.title in (?)', ['Fun Books', 'OK Books']],
-       :include => :topic  }.merge(select_options), 
-      {:select => @select_columns, :on_duplicate_key_update => DUPLICATE_UPDATE_STR})
+       :include => :topic  , 
+       :on_duplicate_key_update => DUPLICATE_UPDATE_STR}.merge(options_two))
       
     # 3 total items
     assert_equal 3, CartItem.count
@@ -99,43 +123,37 @@ class InsertSelectTest < Test::Unit::TestCase
                         :include => { :book => :topic })                      
   end
   
-  def test_insert_select_with_joins_and_limit
+  def test_insert_select_should_import_data_from_one_model_into_another_updating_existing_data_using_joins_and_limit
     #use a join instead of include
-    test_insert_select_with_include :include => nil, :joins => 'inner join topics on topics.id = books.topic_id', :limit => 4
+    options = {:include => nil, :joins => 'inner join topics on topics.id = books.topic_id', :limit => 4}
+    test_insert_select_should_import_data_from_one_model_into_another_updating_existing_data options, options
   end
   
   #test insert select with ignore and duplicate options
-  def test_insert_select_with_duplicate
+  def test_insert_select_should_import_data_from_one_model_into_another_updating_multiple_columns
     
     @cart_copies = Book.count(:all, :conditions => @conditions)
     assert @cart_copies > 0
     
     @time = Time.now
-    CartItem.insert_select(:book, 
-      {:select => ['books.id, ?, ?, ?, ?', @cart.to_param, 1, @time, @time], 
-       :conditions => @conditions }, 
-      {:select => @select_columns})
+    CartItem.insert_select(
+      :from => :book,
+      :select => ['books.id, ?, ?, ?, ?', @cart.to_param, 1, @time, @time], 
+      :conditions => @conditions,
+      :into => @into_columns)
     
     validate_cart_items :total => @cart_copies, :copies => 1
 
     #use on duplicate update
     #this means that the book count should change to 2 and the updated time should be changed to new_time
     new_time = Time.now
-    CartItem.insert_select(:book, 
-      {:select => ['books.id, :cart, :copies, :updated_at, :created_at', {:copies => 2, :cart => @cart, :created_at => new_time, :updated_at => new_time}], 
-       :conditions => @conditions }, 
-      {:select => @select_columns, 
-       :on_duplicate_key_update => [:updated_at, :copies]}) 
-    validate_cart_items :total => @cart_copies, :updated_at => new_time, :copies => 2
+    CartItem.insert_select( 
+       :from => :book,
+       :select => ['books.id, :cart, :copies, :updated_at, :created_at', {:copies => 2, :cart => @cart, :created_at => new_time, :updated_at => new_time}],
+       :conditions => @conditions, 
+       :into => @into_columns,
+       :on_duplicate_key_update => [:updated_at, :copies])
 
-    #ignore all the duplicates
-    #this means that nothing should change
-    CartItem.insert_select(:book, 
-      {:select => "books.id, #{@cart.to_param}, 3, '#{Time.now.to_s(:db)}', '#{Time.now.to_s(:db)}'",
-       :conditions => @conditions }, 
-      {:select => @select_columns, 
-       :ignore => true})
-     
     validate_cart_items :total => @cart_copies, :updated_at => new_time, :copies => 2
 
   end
