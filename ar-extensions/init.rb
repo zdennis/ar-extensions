b3d07698bb9f24330f617cf00ff951a4f432a7ee
Blythe Dunham <blythedunham@gmail.com>
2009-03-13 17:05:33 -0700

    Add rake task to load all available adapter functionality before running ActiveRecord tests

--- ar-extensions/init.rb
+++ ar-extensions/init.rb
@@ -1,7 +1,10 @@
+
 require 'ostruct'
 begin ; require 'active_record' ; rescue LoadError; require 'rubygems'; require 'active_record'; end
 
 $LOAD_PATH.unshift File.expand_path(File.join(File.dirname(__FILE__), 'lib'))
+
+require 'ar-extensions/sql_generation'
 require 'ar-extensions/version'
 require 'ar-extensions/extensions'
 require 'ar-extensions/finder_options'
@@ -9,7 +12,19 @@ require 'ar-extensions/foreign_keys'
 require 'ar-extensions/fulltext'
 require 'ar-extensions/import'
 require 'ar-extensions/insert_select'
+require 'ar-extensions/single_insertion'
 require 'ar-extensions/finders'
 require 'ar-extensions/synchronize'
 require 'ar-extensions/temporary_table'
 require 'ar-extensions/adapters/abstract_adapter'
+
+
+#load all available functionality for specified adapter
+# Ex. ENV['LOAD_ADAPTER_EXTENSIONS'] = 'mysql'
+if ENV['LOAD_ADAPTER_EXTENSIONS']
+  require "active_record/connection_adapters/#{ENV['LOAD_ADAPTER_EXTENSIONS']}_adapter.rb"
+  file_regexp = File.join(File.dirname(__FILE__), 'lib', 'ar-extensions','**',
+                          "#{ENV['LOAD_ADAPTER_EXTENSIONS']}.rb")
+                        
+  Dir.glob(file_regexp){|file| require(file) }
+end
