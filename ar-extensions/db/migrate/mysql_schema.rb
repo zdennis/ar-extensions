524884c0aef11cc3add9dda08a564bc42db12cd7
Zach Dennis <zach.dennis@gmail.com>
2009-04-02 08:49:29 -0400

    added default value to mysql schema, updated db version.rb for tests

--- ar-extensions/db/migrate/mysql_schema.rb
+++ ar-extensions/db/migrate/mysql_schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema.define do
   
   create_table :books, :options=>'ENGINE=MyISAM', :force=>true do |t|
     t.column :title, :string, :null=>false
-    t.column :publisher, :string, :null=>false
+    t.column :publisher, :string, :null=>false, :default => 'Default Publisher'
     t.column :author_name, :string, :null=>false
     t.column :created_at, :datetime
     t.column :created_on, :datetime
--- ar-extensions/db/migrate/version.rb
+++ ar-extensions/db/migrate/version.rb
@@ -1,4 +1,4 @@
 class SchemaInfo < ActiveRecord::Base
   set_table_name 'schema_info'
-  VERSION = 10
+  VERSION = 11
 end
