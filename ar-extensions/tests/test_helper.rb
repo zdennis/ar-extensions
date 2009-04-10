fb65675dfa6e9b0eb91ccad071a52328866b705c
James Herdman <james.herdman@gmail.com>
2009-04-10 18:22:46 -0400

    A little more clean up

--- ar-extensions/tests/test_helper.rb
+++ ar-extensions/tests/test_helper.rb
@@ -12,24 +12,23 @@ end
 
 require "active_record"
 require "active_record/version"
-
-require dir.join("connections", "native_#{ENV['ARE_DB']}", "connection.rb")
-require dir.join("boot").expand_path
-require dir.join("..", "db", "migrate", "version").expand_path
-
 require "mocha"
 require "test/unit"
 require "fileutils"
 require "active_record/fixtures"
 
+require dir.join("connections", "native_#{ENV['ARE_DB']}", "connection.rb")
+require dir.join("boot").expand_path
+require dir.join("..", "db", "migrate", "version").expand_path
+
 ## Load Generic Models
 models_dir = dir.join("models")
 $:.unshift(models_dir)
 
-Dir[models_dir + "/*.rb"].each { |m| require(m) }
+Dir[models_dir.join("*.rb")].each { |m| require(m) }
 
 ## Load Connection Adapter Specific Models
-Dir[models_dir.join(ENV["ARE_DB"].downcase) + "/*.rb" ].each { |m| require(m) }
+Dir[models_dir.join(ENV["ARE_DB"].downcase, "*.rb")].each { |m| require(m) }
 
 # ActiveRecord 1.14.4 (and earlier supported accessor methods for
 # fixture_path as class singleton methods. These tests rely on fixture_path
@@ -44,7 +43,7 @@ if Test::Unit::TestCase.class_variables.include?("@@fixture_path")
   end
 end
 
-# FIXME: stop using rails fixtures and we won"t have to do things like this
+# FIXME stop using Rails fixtures and we won't have to do things like this
 class Fixtures
   def self.cache_for_connection(connection)
     {}
@@ -62,19 +61,13 @@ else
 end
 
 class TestCaseSuperClass #:nodoc:
+  if ActiveRecord::TestCase == TestCaseSuperClass
+    include ActiveRecord::TestFixtures
+  end
+
   self.use_transactional_fixtures = true
   self.use_instantiated_fixtures = false
+  self.fixture_path = dir.join("fixtures")
 end
 
-TestCaseSuperClass.fixture_path = dir.join("fixtures")
-
-module ActiveRecord # :nodoc:
-  module ConnectionAdapters # :nodoc:
-    class PostgreSQLAdapter # :nodoc:
-      def default_sequence_name(table_name, pk = nil)
-        default_pk, default_seq = pk_and_sequence_for(table_name)
-        default_seq || "#{table_name}_#{pk || default_pk || "id"}_seq"
-      end 
-    end
-  end
-end
+#TestCaseSuperClass.fixture_path = dir.join("fixtures")
