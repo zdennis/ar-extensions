3208929f8cbc6971b1f882f609a250145b64918f
James Herdman <james.herdman@gmail.com>
2009-04-10 19:11:10 -0400

    Clean up

--- ar-extensions/tests/run_from_gem.rb
+++ ar-extensions/tests/run_from_gem.rb
@@ -1,15 +1,17 @@
 #!/usr/bin/ruby
 
-dir = File.dirname( __FILE__ )
+dir = File.expand_path(File.dirname(__FILE__))
 
 ADAPTER = ARGV.shift
-ENV['ARE_DB'] = ADAPTER
+ENV["ARE_DB"] = ADAPTER
 
-require 'rubygems'
-gem 'ar-extensions'
-require 'ar-extensions'
+require "rubygems"
 
-require File.expand_path( File.join( dir, 'test_helper' ) )
-Dir[ File.join( dir,  ADAPTER, 'test_*.rb' ) ].each{ |f| require File.expand_path(f) }
-Dir[ File.join( dir, 'test_*.rb' ) ].each{ |f| require File.expand_path(f) unless f == 'test_helper.rb' }
+gem "ar-extensions"
+require "ar-extensions"
 
+require File.join(dir, "test_helper")
+
+Dir[File.join(dir, ADAPTER, "test_*.rb")].each { |f| require(f) }
+
+Dir[File.join(dir, "test_*.rb")].each { |f| require(f) unless f == 'test_helper.rb' }
