27f187d4d21d29c968ea221bc97c130ae54c80dd
James Herdman <james.herdman@gmail.com>
2009-04-10 19:02:17 -0400

    Clean up

--- ar-extensions/tests/prepare.rb
+++ ar-extensions/tests/prepare.rb
@@ -1,9 +1,9 @@
-adapter = ARGV.shift
-ENV['ARE_DB'] = adapter
+adapter       = ARGV.shift
+ENV["ARE_DB"] = adapter
 
-dir = File.dirname( __FILE__ )
-require File.expand_path( File.join( dir, 'test_helper' ) )
+dir = File.dirname(__FILE__)
+require File.expand_path(File.join(dir, 'test_helper'))
 
-require File.join( dir, '../db/migrate/generic_schema' )
-db_schema = File.join( dir, "../db/migrate/#{adapter}_schema.rb" )
-require db_schema if File.exists?( db_schema )
+require File.join(dir, "..", "db", "migrate", "generic_schema")
+db_schema = File.join(dir, "..", "db", "migrate", "#{adapter}_schema.rb")
+require db_schema if File.exists?(db_schema)
