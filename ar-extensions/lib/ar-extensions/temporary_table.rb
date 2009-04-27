e815f390bf798dfa871a3a8dc7765fc2fcaa6a50
James Herdman <james.herdman@gmail.com>
2009-04-27 19:51:22 -0400

    Temporary table models behave just like the model the table is 'like'

--- ar-extensions/lib/ar-extensions/temporary_table.rb
+++ ar-extensions/lib/ar-extensions/temporary_table.rb
@@ -1,14 +1,10 @@
-
 module ActiveRecord::Extensions::TemporaryTableSupport # :nodoc:
   def supports_temporary_tables? #:nodoc:
     true
   end
 end
 
-
 class ActiveRecord::Base
-  @@temporary_table_hsh ||= {}		
-
   # Returns true if the underlying database connection supports temporary tables
   def self.supports_temporary_tables?
     connection.supports_temporary_tables?
@@ -26,99 +22,110 @@ class ActiveRecord::Base
   # * options - the options hash used to define the temporary table. 
   # 
   # ==== Options
-  # * :table_name - the desired name of the temporary table. If not supplied \
-  #   then a name of "temp_" + the current table_name of the current model \
+  # <tt>:table_name</tt>::the desired name of the temporary table. If not supplied 
+  #   then a name of "temp_" + the current table_name of the current model 
   #   will be used.
-  # * :like - the table model you want to base the temporary tables \
-  #   structure off from. If this is not supplied then the table_name of the \
+  # <tt>:like</tt>:: the table model you want to base the temporary tables 
+  #   structure off from. If this is not supplied then the table_name of the 
   #   current model will be used.
-  # * :model_name - the name of the model you want to use for the temporary \
-  #   table. This must be compliant with Ruby's naming conventions for \
-  #   constants. If this is not supplied a rails-generated table name will \
-  #   be created which is based off from the table_name of the temporary table. \
+  # <tt>:model_name</tt>:: the name of the model you want to use for the temporary 
+  #   table. This must be compliant with Ruby's naming conventions for 
+  #   constants. If this is not supplied a rails-generated table name will 
+  #   be created which is based off from the table_name of the temporary table. 
   #   IE: Account.create_temporary_table creates the TempAccount model class
   #
   # ==== Example 1, using defaults
-  #  class Project < ActiveRecord::Base ; end
   #
-  #  Project.create_temporary_table
+  #  class Project < ActiveRecord::Base
+  #  end
+  #
+  #  > t = Project.create_temporary_table
+  #  > t.class
+  #  => "TempProject"
+  #  > t.superclass
+  #  => Project
   #
   # This creates a temporary table named 'temp_projects' and creates a constant
-  # name TempProject. The table structure is copied from the _projects_ table.
+  # name TempProject. The table structure is copied from the 'projects' table. 
+  # TempProject is a subclass of Project as you would expect.
   #
-  # ==== Example 2, using :table_name and :model options
-  #   Project.create_temporary_table :table_name=>'my_projects', :model=>'MyProject'
+  # ==== Example 2, using <tt>:table_name</tt> and <tt>:model options</tt>
+  #
+  #   Project.create_temporary_table :table_name => 'my_projects', :model => 'MyProject'
   #
   # This creates a temporary table named 'my_projects' and creates a constant named
-  # MyProject. The table structure is copied from the _projects_ table.
+  # MyProject. The table structure is copied from the 'projects' table.
   #
-  # ==== Example 3, using :like
-  #   ActiveRecord::Base.create_temporary_table :like=>Project 
+  # ==== Example 3, using <tt>:like</tt>
+  #
+  #   ActiveRecord::Base.create_temporary_table :like => Project 
   #  
   # This is the same as calling Project.create_temporary_table.
   #
   # ==== Example 4, using block form
+  #
   #   Project.create_temporary_table do |t|
   #     # ...
   #   end
   # 
   # Using the block form will automatically drop the temporary table
-  # when the block exits. _t_ which is passed into the block is the temporary
-  # table class. In the above example _t_ equals TempProject. The block form
+  # when the block exits. +t+ which is passed into the block is the temporary
+  # table class. In the above example +t+ equals TempProject. The block form
   # can be used with all of the available options.
   #
   # === See
-  # * drop
+  #
+  # * +drop+
+  #
   ######################################################################
-  def self.create_temporary_table( options={} )
-    options[:table_name] = "temp_#{self.table_name}" unless options[:table_name]
-    options[:like] = self unless options[:like]	
-    options[:temporary] = true if not options[:permanent] and not options.has_key?( :temporary )
-    table_name = options[:table_name]
-    model_name = options[:model_name] || ActiveSupport::Inflector.classify( table_name )	
-    raise Exception.new( "Model #{model_name} already exists! \n" ) if Object.const_defined? model_name 
-    
-    like_table_name = options[:like].table_name || self.table_name
-    sql = "CREATE #{options[:temporary] ? 'TEMPORARY' : ''} TABLE #{table_name} LIKE #{like_table_name}"
-    connection.execute( sql )		
-    
-    eval "class ::#{model_name} < #{ActiveRecord::TemporaryTable.name}
-					set_table_name :#{table_name}
-				end"
+  def self.create_temporary_table(opts={})
+    opts[:temporary]  ||= !opts[:permanent]
+    opts[:like]       ||= self
+    opts[:table_name] ||= "temp_#{self.table_name}"
+    opts[:model_name] ||= ActiveSupport::Inflector.classify(opts[:table_name])
 
-    @@temporary_table_hsh[ model = Object.const_get( model_name ) ] = true
+    if Object.const_defined?(opts[:model_name])
+      raise Exception, "Model #{opts[:model_name]} already exists!"
+    end
+
+    like_table_name = opts[:like].table_name || self.table_name
+
+    connection.execute <<-SQL
+      CREATE #{opts[:temporary] ? 'TEMPORARY' : ''} TABLE #{opts[:table_name]}
+        LIKE #{like_table_name}
+    SQL
+    
+    # Sample evaluation:
+    #
+    #   class ::TempFood < Food
+    #     set_table_name :temp_food
+    #
+    #     def self.drop
+    #       connection.execute "DROP TABLE temp_foo"
+    #       Object.send(:remove_const, self.name.to_sym)
+    #     end
+    #   end
+    class_eval(<<-RUBY, __FILE__, __LINE__)
+      class ::#{opts[:model_name]} < #{self.name}
+        set_table_name :#{opts[:table_name]}
+
+        def self.drop
+          connection.execute "DROP TABLE #{opts[:table_name]};"
+          Object.send(:remove_const, self.name.to_sym)
+        end
+      end
+    RUBY
+
+    model = Object.const_get(opts[:model_name])
+
     if block_given?
-      yield model
-      model.drop
-      nil
+      begin
+        yield(model)
+      ensure
+        model.drop
+      end
     else
-      model
+      return model
     end
   end
-
 end
-
-class ActiveRecord::TemporaryTable < ActiveRecord::Base
-  
-  # Drops a temporary table from the database and removes
-  # the temporary table constant.
-  #
-  # ==== Example
-  #   Project.create_temporary_table
-  #   Object.const_defined?( :TempProject ) # => true
-  #   TempProject.drop
-  #   Object.const_defined?( :TempProject ) # => false
-  #
-  def self.drop
-    if @@temporary_table_hsh[ self ]
-      sql = 'DROP TABLE ' + self.table_name + ';'
-      connection.execute( sql )
-      Object.send( :remove_const, self.name.to_sym )
-      @@temporary_table_hsh.delete( self )
-    else
-      raise StandardError.new( "Trying to drop nonexistance temporary table: #{self.name}" )
-    end
-  end
-  
-end
-
--- ar-extensions/tests/test_temporary_tables.rb
+++ ar-extensions/tests/test_temporary_tables.rb
@@ -1,117 +1,86 @@
-require File.expand_path( File.join( File.dirname( __FILE__ ), 'test_helper' ) )
+require File.expand_path(File.join(File.dirname(__FILE__), "test_helper"))
 
-class TemporaryTableCRUDTest< TestCaseSuperClass
+class TestTemporaryTableExtension < TestCaseSuperClass
+  def setup
+    unless Book.supports_temporary_tables?
+      raise "Your database adapter doesn't support temporary tables"
+    end
+    super
+  end
 
   def test_creating_a_temporary_table_creates_a_model
-    unless Book.supports_temporary_tables?
-      STDERR.puts "test_creating_a_temporary_table_creates_a_model is not testing, since your database adapter doesn't support temporary tables."
-    else    
-      model_class = Book.create_temporary_table
-      assert_kind_of ActiveRecord::TemporaryTable, model_class.allocate
-      model_class.drop
-    end
+    model_class = Book.create_temporary_table
+    assert_kind_of Book, model_class.allocate
+  ensure
+    model_class.drop
   end
- 
+
   def test_creating_a_temporary_table_creates_a_toplevel_namespace_accessible_model
-    unless Book.supports_temporary_tables?
-      STDERR.puts "test_creating_a_temporary_table_creates_a_toplevel_namespace_accessible_model is not testing, since your database adapter doesn't support temporary tables."
-    else    
-      model_class = Book.create_temporary_table
-      assert TempBook
-      model_class.drop
-    end
+    model_class = Book.create_temporary_table
+
+    assert TempBook
+  ensure
+    model_class.drop
   end
 
   def test_model_can_be_dropped
-    unless Book.supports_temporary_tables?
-      STDERR.puts "test_model_can_be_dropped is not testing, since your database adapter doesn't support temporary tables."
-    else        
-      model_class = Book.create_temporary_table
-      model_class.drop
-      assert !Object.const_defined?( model_class.name) 
-    end
+    model_class = Book.create_temporary_table
+    model_class.drop
+
+    assert !Object.const_defined?(model_class.name) 
   end
 
   def test_creating_a_temporary_table_with_predefined_name
-    unless Book.supports_temporary_tables?
-      STDERR.puts "test_creating_a_temporary_table_with_predefined_name is not testing, since your database adapter doesn't support temporary tables."
-    else        
-      model_class = Book.create_temporary_table :table_name=>'xyz'
-      assert_equal 'xyz', model_class.table_name
-      model_class.drop
-      assert !Object.const_defined?( model_class.name )
-    end
+    model_class = Book.create_temporary_table :table_name => "xyz"
+    assert_equal "xyz", model_class.table_name
+  ensure
+    model_class.drop
+  end
+
+  def test_temp_table_drops_after_block
+    temp_model = nil
+    Book.create_temporary_table { |t| temp_model = t }
+    assert !Object.const_defined?(temp_model.name.to_sym)
   end
 
   def test_creating_a_temporary_table_with_a_block
-    unless Book.supports_temporary_tables?
-      STDERR.puts "test_creating_a_temporary_table_with_a_block is not testing, since your database adapter doesn't support temporary tables."
-    else        
-      model_name = ''
-      Book.create_temporary_table do |model_clazz|
-        model_name = model_clazz.name
-        rec = model_clazz.create :author_name=>"Zach Dennis", :title => "SomeBook", :publisher=>"SomePublisher"
-        assert_equal 1, model_clazz.count
-      end
-      assert !Object.const_defined?( model_name )
+    Book.create_temporary_table do |model_clazz|
+      model_clazz.create(
+        :author_name => "Zach Dennis",
+        :title       => "SomeBook",
+        :publisher   => "SomePublisher"
+      )
+
+      assert_equal 1, model_clazz.count
     end
   end
 
-  def test_creating_a_temporary_table_with_specified_model_and_table_names
-    unless Book.supports_temporary_tables?
-      STDERR.puts "test_creating_a_temporary_table_with_specified_model_and_table_names is not testing, since your database adapter doesn't support temporary tables."
-    else
-      model_name, table_name = 'TestTable', 'some_table'
-      model_class = Book.create_temporary_table :model_name=>model_name, :table_name=>table_name
-      assert_equal model_name, model_class.name
-      assert_equal table_name, model_class.table_name
-      model_class.drop
+  def test_creating_a_temporary_table_with_specified_model_name
+    Book.create_temporary_table(:model_name => "TestTable") do |t|
+      assert_equal t.name, "TestTable"
+    end
+  end
+
+  def test_creating_a_temporary_table_with_specified_table_name
+    Book.create_temporary_table(:table_name => "my_test_books") do |t|
+      assert_equal t.table_name, "my_test_books"
     end
   end
 
   def test_temporary_table_doesnt_persist_across_connections
-    unless Book.supports_temporary_tables?
-      STDERR.puts "test_temporary_table_doesnt_persist_across_connections is not testing, since your database adapter doesn't support temporary tables."
-    else
-      model_class = Book.create_temporary_table
-      assert_kind_of ActiveRecord::TemporaryTable, model_class.allocate
-      
-      # model_class.connection.reconnect!
-      # assert_raises( ActiveRecord::StatementInvalid ){ model_class.count }
-      model_class.drop
-    end
-  end
-
-end
-
-
-class LikeTableCRUDTest< TestCaseSuperClass
-#  self.fixture_path = File.join( File.dirname( __FILE__ ), 'fixtures/unit/temporary_table' )
-
-  def test_creating_like_table
-    unless Book.supports_temporary_tables?
-      STDERR.puts "test_creating_like_table isn't testing, since your database adapter doesn't support temporary tables."
-    else
-      model_class = Book.create_temporary_table :permanent=>true
-      assert_kind_of ActiveRecord::TemporaryTable, model_class.allocate
-      model_class.drop
-    end
+    model_class = Book.create_temporary_table
+    assert_kind_of Book, model_class.allocate
+  ensure
+    model_class.drop
   end
 
   def test_permanent_table_persists_across_connections
-    unless Book.supports_temporary_tables?
-      STDERR.puts "test_permanent_table_persists_across_connections is not testing, since your database adapter doesn't support temporary tables."
-    else
-      model_class = Book.create_temporary_table :permanent=>true
-      assert_kind_of ActiveRecord::TemporaryTable, model_class.allocate
-      
-      model_class.connection.reconnect!
-      assert_equal 0, model_class.count
-      
-      model_class.drop    
-      assert !Object.const_defined?( model_class.name) 
-    end
+    model_class = Book.create_temporary_table :permanent => true
+    model_class.allocate
+    model_class.connection.reconnect!
+
+    assert_equal 0, model_class.count # if we can count, it persisted
+  ensure
+    model_class.drop
   end
-
-
 end
