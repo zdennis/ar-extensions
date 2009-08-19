78f4c43f25b209ebe2990f6720545e112c69451c
Zach Dennis <zach.dennis@gmail.com>
2009-08-18 21:10:48 -0400

    Update temporary table test to not puke when running sqlite3 tests

--- ar-extensions/tests/test_temporary_tables.rb
+++ ar-extensions/tests/test_temporary_tables.rb
@@ -1,86 +1,93 @@
 require File.expand_path(File.join(File.dirname(__FILE__), "test_helper"))
 
 class TestTemporaryTableExtension < TestCaseSuperClass
-  def setup
-    unless Book.supports_temporary_tables?
-      raise "Your database adapter doesn't support temporary tables"
+  def self.test(description, &blk)
+    if Book.supports_temporary_tables?
+      define_method("test_#{description}", &blk)
+    else
+      puts "*** no db support for temporary tables, test not running: #{description}"
     end
-    super
   end
 
-  def test_creating_a_temporary_table_creates_a_model
-    model_class = Book.create_temporary_table
-    assert_kind_of Book, model_class.allocate
-  ensure
-    model_class.drop
+  test "creating a temporary model creates a model" do
+    begin
+      model_class = Book.create_temporary_table
+      assert_kind_of Book, model_class.allocate
+    ensure
+      model_class.drop
+    end
   end
-
-  def test_creating_a_temporary_table_creates_a_toplevel_namespace_accessible_model
-    model_class = Book.create_temporary_table
-
-    assert TempBook
-  ensure
-    model_class.drop
+  
+  test "creating_a_temporary_table_creates_a_toplevel_namespace_accessible_model" do
+    begin
+      model_class = Book.create_temporary_table
+      assert TempBook
+    ensure
+      model_class.drop
+    end
   end
-
-  def test_model_can_be_dropped
+  
+  test "model can be dropped" do
     model_class = Book.create_temporary_table
     model_class.drop
-
     assert !Object.const_defined?(model_class.name) 
   end
-
-  def test_creating_a_temporary_table_with_predefined_name
-    model_class = Book.create_temporary_table :table_name => "xyz"
-    assert_equal "xyz", model_class.table_name
-  ensure
-    model_class.drop
+  
+  test "creating_a_temporary_table_with_predefined_name" do
+    begin
+      model_class = Book.create_temporary_table :table_name => "xyz"
+      assert_equal "xyz", model_class.table_name
+    ensure
+      model_class.drop
+    end
   end
-
-  def test_temp_table_drops_after_block
+  
+  test "temporary table drops after block" do
     temp_model = nil
     Book.create_temporary_table { |t| temp_model = t }
     assert !Object.const_defined?(temp_model.name.to_sym)
   end
-
-  def test_creating_a_temporary_table_with_a_block
+  
+  test "creating a temporary table with a block" do
     Book.create_temporary_table do |model_clazz|
       model_clazz.create(
         :author_name => "Zach Dennis",
         :title       => "SomeBook",
         :publisher   => "SomePublisher"
       )
-
       assert_equal 1, model_clazz.count
     end
   end
-
-  def test_creating_a_temporary_table_with_specified_model_name
+  
+  test "creating a temporary table with a specified model name" do
     Book.create_temporary_table(:model_name => "TestTable") do |t|
       assert_equal t.name, "TestTable"
     end
   end
-
-  def test_creating_a_temporary_table_with_specified_table_name
+  
+  test "creating a temporary table with specified table name" do
     Book.create_temporary_table(:table_name => "my_test_books") do |t|
       assert_equal t.table_name, "my_test_books"
     end
   end
-
-  def test_temporary_table_doesnt_persist_across_connections
-    model_class = Book.create_temporary_table
-    assert_kind_of Book, model_class.allocate
-  ensure
-    model_class.drop
+  
+  test "temporary table doesn't persist across connections" do
+    begin
+      model_class = Book.create_temporary_table
+      assert_kind_of Book, model_class.allocate
+    ensure
+      model_class.drop
+    end
   end
-
-  def test_permanent_table_persists_across_connections
-    model_class = Book.create_temporary_table :permanent => true
-    model_class.allocate
-    model_class.connection.reconnect!
-
-    assert_equal 0, model_class.count # if we can count, it persisted
-  ensure
-    model_class.drop
+  
+  test "permant table persists across connections" do
+    begin
+      model_class = Book.create_temporary_table :permanent => true
+      model_class.allocate
+      model_class.connection.reconnect!
+      assert_equal 0, model_class.count # if we can count, it persisted
+    ensure
+      model_class.drop
+    end
   end
 end
