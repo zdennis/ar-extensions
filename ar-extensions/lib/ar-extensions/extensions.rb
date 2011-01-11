4d5c3e364c0e55325cdda39c5e81e9d5c81d9153
Greg Hazel <ghazel@gmail.com>
2011-01-10 18:45:55 -0800

    Updating code to remove to_a deprecation warning

--- ar-extensions/lib/ar-extensions/extensions.rb
+++ ar-extensions/lib/ar-extensions/extensions.rb
@@ -256,7 +256,14 @@ module ActiveRecord::Extensions
     STARTS_WITH_RGX = /(.+)_starts_with$/
     ENDS_WITH_RGX =  /(.+)_ends_with$/
     def self.process( key, val, caller )
-      values = [*val]
+      values = case val
+        when NilClass
+          []
+        when Array
+          val
+       else
+         [val]
+      end
       case key.to_s
       when LIKE_RGX
         str = values.collect do |v|
