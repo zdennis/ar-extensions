b287b55dc589a8441e6a178f017b13fbc9370352
Samuel Oey <oey@ubilabs.net>
2012-08-01 10:38:40 +0200

    adds mysql2 compatibility for mysql adapter

--- ar-extensions/lib/ar-extensions.rb
+++ ar-extensions/lib/ar-extensions.rb
@@ -3,3 +3,11 @@ require 'active_record' # ActiveRecord loads the Benchmark library automatically
 require 'active_record/version'
 
 require File.expand_path(File.join( File.dirname( __FILE__ ), '..', 'init.rb' ))
+
+MYSQL_ADAPTER_CLASS = if defined? ActiveRecord::ConnectionAdapters::MysqlAdapter
+  ActiveRecord::ConnectionAdapters::MysqlAdapter
+elsif defined? ActiveRecord::ConnectionAdapters::Mysql2Adapter
+  ActiveRecord::ConnectionAdapters::Mysql2Adapter
+else
+  raise "ar-extensions: Missing or unknown ActiveRecord::ConnectionAdapter"
+end
--- ar-extensions/lib/ar-extensions/adapters/mysql.rb
+++ ar-extensions/lib/ar-extensions/adapters/mysql.rb
@@ -1,9 +1,9 @@
-ActiveRecord::ConnectionAdapters::MysqlAdapter.class_eval do
+MYSQL_ADAPTER_CLASS.class_eval do
   # Returns the maximum number of bytes that the server will allow
   # in a single packet
   def max_allowed_packet # :nodoc:
     result = execute( "SHOW VARIABLES like 'max_allowed_packet';" )
-    result.fetch_row[1].to_i
+    result.respond_to?(:fetch_row) ? result.fetch_row[1].to_i : result.first[1]
   end
 
   def rollup_sql; " WITH ROLLUP "; end
